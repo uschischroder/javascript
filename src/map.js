165693c754d9bdef6fc49af6e90f0e4cd3b5bd6d
Luiz Eduardo <dudu.christo@gmail.com>
2018-02-08 16:49:50 +0100

    JS-08 - Decouple Random from Map

--- src/map.js
+++ src/map.js
@@ -1,25 +1,14 @@
-
 var getAvailableOperations = function() {
   return availableOperations;
-}	
+};
 
 var applyOperation = function (arr, operation, factor) {
   let func = availableOperations[operation];
   return func && arr.map(func(factor));
-}
-
-const getRandom = () => Math.random()*2001 - 1000;
-
-function generate(n) {
-  if (n == undefined || n == null) n = 1000;
-
-  var arr = new Array(n);
-  return arr.map(getRandom);	
-}
+};
 
 const add = (factor) => (x) => x + factor;
 const product = (factor) => (x) => x*factor;
-
 
 var availableOperations = {
   add: add,
@@ -27,7 +16,6 @@ var availableOperations = {
 };
 
 module.exports = {
-  generate,
   applyOperation,
   getAvailableOperations
 };
---
+++ test/random.spec.js
@@ -0,0 +1,33 @@
+const { assert } = require('chai');
+var  Random = require('../src/random');
+
+describe('Random', function() {
+
+    describe('generate()', function(){
+
+        it('should return a non empty array', function() {
+            let arr = Random.generate();
+            assert.isArray(arr);
+        });
+
+        it('should return an array with correct default size', function() {
+            let arr = Random.generate();
+            assert.lengthOf(arr, 1000);
+        });
+
+        it('should return an array with correct user defined size', function() {
+            let arr = Random.generate(10);
+            assert.lengthOf(arr, 10);
+        });
+
+        it('should return default size if size param is null or undefined', function() {
+            let arr;
+            arr = Random.generate(null);
+            assert.lengthOf(arr, 1000);
+
+            arr = Random.generate(undefined);
+            assert.lengthOf(arr, 1000);
+        });
+
+    });
+});
