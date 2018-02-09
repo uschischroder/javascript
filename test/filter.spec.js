1db052bd0ef84f961141aab82b28edad226cc381
Luiz Eduardo <dudu.christo@gmail.com>
2018-02-09 12:15:31 +0100

    JS-07 - Adds unit test for multiple (5 and 22) filter type

--- test/filter.spec.js
+++ test/filter.spec.js
@@ -31,6 +31,15 @@ describe('Filter', function() {
             assert.isNotNull(result);
             assert.isArray(result);
             assert.deepEqual(result, [2,4,6,8,10]);
+
+            result = Filter.applyFilter([1,2,3,4,5,6,7,8,9,10000], 'multiple', 22)
+            assert.isArray(result);
+            assert.lengthOf(result, 0);
+
+            result = Filter.applyFilter([125,2,30,4,5,60,77,8,9,1000, 250000], 'multiple', 5)
+            assert.isNotNull(result);
+            assert.isArray(result);
+            assert.deepEqual(result, [125, 30, 5, 60, 1000, 250000]);
         })
     });
 });
